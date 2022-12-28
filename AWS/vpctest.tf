461ef6c289deedcbc5dd73798158033aee74af53
KimGilSu <hwanrim00@gmail.com>
2022-12-29 00:44:11 +0900

    Apply .gitignore

--- AWS/vpctest.tf
+++ AWS/vpctest.tf
@@ -1,15 +1,15 @@
-resource "aws_vpc" "tf_vpc" {
-  cidr_block = "192.168.0.0/16"
+# resource "aws_vpc" "tf_vpc" {
+#   cidr_block = "192.168.0.0/16"
 
-  tags = {
-    Name = "terraform_test"
-  }
-}
-resource "aws_subnet" "tf_public_sb_a" {
-  vpc_id = aws_vpc.tf_vpc.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "ap-northeast-2a"
-  tags = {
-    Name = "subnet-terraform"
-  }
-}
+#   tags = {
+#     Name = "terraform_test"
+#   }
+# }
+# resource "aws_subnet" "tf_public_sb_a" {
+#   vpc_id = aws_vpc.tf_vpc.id
+#   cidr_block = "192.168.1.0/24"
+#   availability_zone = "ap-northeast-2a"
+#   tags = {
+#     Name = "subnet-terraform"
+#   }
+# }
