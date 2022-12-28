cbf3824c2cc4afd29bed9578f038dccaf3a94456
KimGilSu <hwanrim00@gmail.com>
2022-12-29 00:36:58 +0900

    Update AWS Route table

--- AWS/vpc.tf
+++ AWS/vpc.tf
@@ -13,3 +13,33 @@ resource "aws_subnet" "tf_public_sb_a" {
     Name = "subnet-terraform"
   }
 }
+
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf_igw"
+  }
+}
+
+
+// 라우트 테이블
+resource "aws_route_table" "tf_pulibc_rt" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf_igw.id
+  }
+
+
+  tags = {
+    Name = "tf_pulibc_rt"
+  }
+}
+
+resource "aws_route_table_association" "tf_pulibc_route_table_association" {
+  subnet_id = aws_subnet.tf_public_sb_a.id
+  route_table_id = aws_route_table.tf_pulibc_rt.id
+  
+}
