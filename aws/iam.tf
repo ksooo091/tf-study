b153f23b7055b45a19be36e85a3d5b16ea38c0af
KimGilSu <hwanrim00@gmail.com>
2023-01-03 23:11:18 +0900

    iam commit

---
+++ aws/iam.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_user" "tfstudyuser"{
+ name = "tfstudyuser"
+}
+
+resource "aws_iam_access_key" "tfstudyuser" {
+  user = aws_iam_user.tfstudyuser.name
+}
+
+resource "aws_iam_group" "iamg_tfstudy"{
+ name = "tfstudy"
+}
